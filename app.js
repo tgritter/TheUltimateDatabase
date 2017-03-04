df95e4feb6004ece298240c9eb01f60b160a853e
tgritter <travis.gritter@gmail.com>
2017-03-04 02:38:17 -0800

    Update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var monk = require('monk');
 var uri = 'localhost:27017/UP';
-var uri = 'mongodb://heroku_9326dm9f:fkrpp027tq4dt4jrvrpfnu465d@ds019796.mlab.com:19796/heroku_9326dm9f';
+var uri = 'mongodb://heroku_clnt20n7:t2s0j5s6ac669kvsi7o9poa761@ds119020.mlab.com:19020/heroku_clnt20n7';
 var db = monk(uri);
 var test = 'test';
 
